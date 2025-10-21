ed63b6eb30c24851629b83a368f24f83d02f232b
Samujal Phukan <samujalphukan@yahoo.com>
2025-10-21 23:19:27 +0530

    Change API base URL to production endpoint

--- front/src/utils/api.js
+++ front/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/email",
+    baseURL: "https://email-backend-gray.vercel.app",
 });
 
 export const sendEmail = (data) => API.post("/send", data);
